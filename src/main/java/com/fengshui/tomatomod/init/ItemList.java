44575540ec9690ccc29e1e312d22d9c85b0be283
clxxu <clx@princeton.edu>
2020-08-25 15:47:45 -0700

    first commit

--- src/main/java/com/fengshui/tomatomod/init/ItemList.java
+++ src/main/java/com/fengshui/tomatomod/init/ItemList.java
@@ -2,6 +2,8 @@ package com.fengshui.tomatomod.init;
 
 import com.fengshui.tomatomod.Main;
 import com.fengshui.tomatomod.items.ModdingToolItem;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Food;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,4 +14,12 @@ public class ItemList {
 
     public static final RegistryObject<Item> MODDING_TOOL = ITEMS.register("modding_tool",
             () -> new ModdingToolItem(new Item.Properties().group(Main.TAB)));
+
+    public static final RegistryObject<Item> TOMATO_SEEDS = ITEMS.register("tomato_crop",
+            () -> new BlockItem(BlockList.TOMATO_CROP.get(),
+                    new Item.Properties().group(Main.TAB)));
+
+    public static final RegistryObject<Item> TOMATO = ITEMS.register("tomato",
+            () -> new Item(new Item.Properties().group(Main.TAB)
+                    .food(new Food.Builder().hunger(4).saturation(3.0f).build())));
 }
